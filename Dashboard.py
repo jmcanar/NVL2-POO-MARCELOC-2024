d63e3befe03d40f30aadd70feac8a93681288af8
ASUS TUF GAMING <jm.canarj@uea.edu.ec>
2024-07-21 14:23:33 -0500

    tecnicas de programacion

---
+++ Dashboard.py
@@ -0,0 +1,52 @@
+import os
+
+
+def mostrar_codigo(ruta_script):
+    # Asegúrate de que la ruta al script es absoluta
+    ruta_script_absoluta = os.path.abspath(ruta_script)
+    try:
+        with open(ruta_script_absoluta, 'r') as archivo:
+            print(f"\n--- Código de {ruta_script} ---\n")
+            print(archivo.read())
+    except FileNotFoundError:
+        print("El archivo no se encontró.")
+    except Exception as e:
+        print(f"Ocurrió un error al leer el archivo: {e}")
+
+
+def mostrar_menu():
+    # Define la ruta base donde se encuentra el dashboard.py
+    ruta_base = os.path.dirname(__file__)
+
+    opciones = {
+        '1': 'programacion Orientada a Objetos (POO).py',
+        '2': 'programacion tradicional.py',
+        '3': 'tecnicas_de_POO.py',
+        '4': 'SEMANA 4/situciones_mundo_real.py',
+        '5': 'SEMANA 5/area_del_triangulo.py',
+        '6': 'SEMANA 6/Semana_6.py',
+        '7': 'SEMANA 7/contructores_destructores.py',
+        # Agrega aquí el resto de las rutas de los scripts
+    }
+
+    while True:
+        print("\nMenu Principal - Dashboard")
+        # Imprime las opciones del menú
+        for key in opciones:
+            print(f"{key} - {opciones[key]}")
+        print("0 - Salir")
+
+        eleccion = input("Elige un script para ver su código o '0' para salir: ")
+        if eleccion == '0':
+            break
+        elif eleccion in opciones:
+            # Asegura que el path sea absoluto
+            ruta_script = os.path.join(ruta_base, opciones[eleccion])
+            mostrar_codigo(ruta_script)
+        else:
+            print("Opción no válida. Por favor, intenta de nuevo.")
+
+
+# Ejecutar el dashboard
+if __name__ == "__main__":
+    mostrar_menu()
